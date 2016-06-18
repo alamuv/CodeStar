bc905128afac91a24acb5d935e7cf11dd8689955
Alamelu Palaniappan <alam.palaniappan@gmail.com>
2016-06-18 09:21:04 -0700

    add star count to repos

--- client/app/view/RepoView.js
+++ client/app/view/RepoView.js
@@ -2,7 +2,7 @@ CodeStars.Views.RepoView = Backbone.View.extend({
   //model is photo
   className: 'repo',
   tagName: 'li',
-  template: _.template('<%= name %>'),
+  template: _.template('<%= name %>   StarCount:<%= starCount %>'),
   // events: {
   //   click: function() {
   //     this.model.setCurrentPhoto();
@@ -11,7 +11,7 @@ CodeStars.Views.RepoView = Backbone.View.extend({
 
   render: function(){
     // console.log(this.model.get('name'));
-    var listEntry = this.template({ name: this.model.get('name')});
+    var listEntry = this.template({ name: this.model.get('name'), starCount: this.model.get('stargazers_count')});
     return this.$el.html(listEntry);
   }
 });
