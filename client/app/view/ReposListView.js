fe660eab860da5943ce51e1e0b6bede71c14d44a
Alamelu Palaniappan <alam.palaniappan@gmail.com>
2016-06-18 09:33:30 -0700

    comput total star count

--- client/app/view/ReposListView.js
+++ client/app/view/ReposListView.js
@@ -9,8 +9,10 @@ CodeStars.Views.ReposListView = Backbone.View.extend({
 
   render: function() {
     this.$el.children().detach();
-
-    return this.$el.addClass('reposList').html('<h3>List of Repositories</h3>').append(this.collection.map(function(repo) {
+    var totalStars = 0;
+    console.log(this.collection);
+    return this.$el.addClass('reposList').html('<h3>List of Repositories<%=totalStars%></h3>').append(this.collection.map(function(repo) {
+      totalStars += repo.attributes.stargazers_count;
       return new CodeStars.Views.RepoView({ model: repo }).render();
     }));
   }
